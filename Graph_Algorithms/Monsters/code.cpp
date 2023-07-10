3f6bff9f3621e52507d3383af25bbbf788db21d4
Arijit <sontu1729@gmail.com>
2023-07-10 13:40:50 +0530

    TLE

--- Graph_Algorithms/Monsters/code.cpp
+++ Graph_Algorithms/Monsters/code.cpp
@@ -6,163 +6,168 @@ using namespace std;
 
 #define ll long long
 #define MOD 1000000007
-#define INPUT(N, arr) for (int i = 0; i < N; cin >> arr[i], i++)
+#define INPUT(N, arr) for (int i = 0; i < N; cin >> arr[i], i++);
 #define newline '\n'
 #define FOR(i, a, b) for (int i = a; i < b; i++)
-#define blankchar ' '
+#define blankChar ' '
 #define Point pair<int, int>
+#define ADD(a, b) ((a == LONG_MAX or b == LONG_MAX) ? LONG_MAX : a + b)
 // tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
+int n, m;
+vector<pair<int, int>> Monsters;
+pair<int, int> Me;
+bool arr[1001][1001];
+int gate[1001][1001];
+Point parent[1001][1001];
 
+void fill_gate() {
+    for (int i = 0; i < 1001; i ++) {
+        for (int j = 0; j < 1001; j++) gate[i][j] = INT_MAX;
+    }
+}
+
+
+bool is_inside(int x, int y) {
+    return (x >= 0 and y >= 0 and x < n and y < m and arr[x][y]);
+}
+
+bool is_gate(int x, int y) {
+    return (x == 0 or y == 0 or x == n - 1 or y == m - 1);
+}
+
+void bfs(pair<int, int> monster) {
+    vector<vector<bool>> is_visited(n, vector<bool>(m, false));
+
+    queue<pair<Point, int>> Q;
+    Q.push(make_pair(monster, 0));
+
+    while (not Q.empty()) {
+        auto top = Q.front();
+        auto pos = top.first;
+        auto level = top.second;
+        Q.pop();
+        is_visited[pos.first][pos.second] = true;
+
+        if (is_gate(pos.first, pos.second)) {
+            gate[pos.first][pos.second] = min(gate[pos.first][pos.second], level);
+        }
+        for (int i = -1; i < 2; i ++) {
+            for (int j = 1 - abs(i); j >= abs(i) - 1; j -= 2) {
+
+                int x = pos.first + i;
+                int y = pos.second + j;
+                if (is_inside(x, y) and not is_visited[x][y]) {
+                    is_visited[x][y] = true;
+                    Q.push(make_pair(make_pair(x, y), level + 1));
+                }
+            }
+        }
+    }
+}
+
+void done(int x, int y) {
+    stack<char> Path;
+
+    while (parent[x][y].first != -1) {
+        if (parent[x][y].first == x) {
+            if (parent[x][y].second < y) {
+                Path.push('R');
+            }
+            else {
+                Path.push('L');
+            }
+        }
+        else {
+            if (parent[x][y].first < x) {
+                Path.push('D');
+            }
+            else {
+                Path.push('U');
+            }
+        }
+
+        auto p = parent[x][y];
+        x = p.first;
+        y = p.second;
+    }
+
+    cout << "YES" << newline;
+    cout << Path.size() << newline;
+    for (; not Path.empty(); Path.pop()) {
+        cout << Path.top();
+    }
+    cout << endl;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int height, width;
-    cin >> height >> width;
+    fill_gate();
 
     char c;
-    vector<vector<bool>> Graph(height + 1, vector<bool>(width + 1, 0));
-    vector<vector<bool>> is_visited(height + 1, vector<bool>(width + 1, false));
-    vector<vector<Point>> parent(height + 1, vector<Point>(width + 1));
+    cin >> n >> m;
 
-    set<Point> Monsters, Me;
-
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= width; j++) {
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> c;
-
-            Graph[i][j] = (c != '#');
-
-            if (c == 'A') {
-                Me.insert({i, j});
-                is_visited[i][j] = true;
+            arr[i][j] = (c == '.');
+            if (c == 'M') {
+                Monsters.push_back({i, j});
             }
-            else if (c == 'M') {
-                Monsters.insert({i, j});
+            else if (c == 'A') {
+                Me = {i, j};
             }
         }
     }
 
 
-
-
-    while (true) {
-
-    //     set<Point> temp;
-
-    //     for (auto it = Monsters.begin(); it != Monsters.end(); it ++) {
-
-    //         int x = it->first, y = it->second;
-
-    //         if (x - 1 > 0) {
-    //             if (y - 1 > 0) {
-    //                 temp.insert(x - 1, y - 1);
-    //             }
-    //             if (y + 1 <= width) {
-    //                 temp.insert(x - 1, y + 1);
-    //             }
-    //         }
-
-    //         if (x + 1 <= height) {
-    //             if (y - 1 > 0) {
-    //                 temp.insert(x + 1, y - 1);
-    //             }
-    //             if (y + 1 <= width) {
-    //                 temp.insert(x + 1, y + 1);
-    //             }
-
-    //         }
-
-    //     }
-
-    //     Monsters = temp;
-
-    //     set<Point> temp2;
-    //     int final_x, final_y;
-
-    //     for (auto it = Me.begin(); it != Me.begin(); it ++) {
-    //         int x = it->first, y = it->second;
-
-    //         bool is_way = false;
-
-    //         if (x - 1 > 0) {
-    //             if (y - 1 > 0) {
-    //                 if (Graph[x - 1][y - 1] and Monsters.find({x - 1, y - 1}) != Monsters.end() and not is_visited[x - 1][y - 1]) {
-    //                     temp2.insert(x - 1, y - 1);
-    //                     is_visited[x - 1][y - 1];
-    //                     is_way = true;
-    //                     parent[x - 1][y - 1] = {x, y};
-    //                 }
-
-    //             }
-    //             else {
-    //                 final_x = x;
-    //                 final_y = y;
-    //                 break;
-    //             }
-    //             if (y + 1 <= width) {
-    //                 if (Graph[x - 1][y + 1] and Monsters.find({x - 1, y + 1}) != Monsters.end() and not is_visited[x - 1][y + 1]) {
-    //                     temp2.insert(x - 1, y + 1);
-    //                     is_visited[x - 1][y + 1];
-    //                     is_way = true;
-    //                     parent[x - 1][y + 1] = {x, y};
-    //                 }
-
-    //             }
-    //             else {
-    //                 final_x = x;
-    //                 final_y = y;
-    //                 break;
-    //             }
-    //         }
-
-    //         if (x + 1 <= height) {
-
-    //             if (y - 1 > 0 ) {
-    //                 if (Graph[x + 1][y - 1] and Monsters.find({x + 1, y - 1}) != Monsters.end() and not is_visited[x + 1][y - 1]) {
-    //                     temp2.insert(x + 1, y - 1);
-    //                     is_visited[x + 1][y - 1];
-    //                     is_way = true;
-    //                     parent[x + 1][y - 1] = {x, y};
-    //                 }
-
-    //             }
-    //             else {
-    //                 final_x = x;
-    //                 final_y = y;
-    //                 break;
-    //             }
-
-    //             if (y + 1 <= width) {
-    //                 if (Graph[x + 1][y + 1] and Monsters.find({x + 1, y + 1}) != Monsters.end() and not is_visited[x + 1][y + 1]) {
-    //                     temp2.insert(x + 1, y + 1);
-    //                     is_visited[x + 1][y + 1];
-    //                     is_way = true;
-    //                     parent[x + 1][y + 1] = {x, y};
-    //                 }
-
-    //             }
-    //             else {
-    //                 final_x = x;
-    //                 final_y = y;
-    //                 break;
-    //             }
-
-    //         }
-
-    //         if (! is_way) {
-    //             cout << "NO" << newline;
-    //             return 0;
-    //         }
-
-    //     }
-
-    //     Me = temp2;
+    for (auto monster : Monsters) {
+        bfs(monster);
     }
 
+
+    vector<vector<bool>> is_visited(n, vector<bool>(m, false));
+
+    queue<pair<Point, int>> Q;
+    Q.push(make_pair(Me, 0));
+    parent[Me.first][Me.second] = { -1, -1};
+
+    while (not Q.empty()) {
+        auto top = Q.front();
+        auto pos = top.first;
+        auto level = top.second;
+
+        // cout << pos.first << blankChar << pos.second << endl;
+
+        Q.pop();
+        is_visited[pos.first][pos.second] = true;
+
+        if (is_gate(pos.first, pos.second)) {
+            if (level < gate[pos.first][pos.second]) {
+                done(pos.first, pos.second);
+                return 0;
+            }
+        }
+
+        else {
+            for (int i = -1; i < 2; i ++) {
+
+                for (int j = 1 - abs(i); j >= abs(i) - 1; j -= 2) {
+                    int x = pos.first + i;
+                    int y = pos.second + j;
+                    if (is_inside(x, y) and not is_visited[x][y]) {
+                        parent[x][y] = pos;
+                        is_visited[x][y] = true;
+                        Q.push(make_pair(make_pair(x, y), level + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "NO" << endl;
 
     return 0;
 }
